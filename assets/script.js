1389f2ab5e17c49941c60dd3356e6a96a3dff381
einalem4 <melanie@codehangar.io>
2021-01-29 16:19:15 -0700

    display current temperature

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,22 @@
 var cityInput = document.querySelector("#city-input").value
 var searchButton = document.querySelector("#search-button")
+var mainWeather = document.querySelector("#weather")
+var mainTempEl = document.querySelector("#temperature")
 
+// current weather
 function currentWeather(city) {
 
     fetch(
-        'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=77405a385bebafa9f315f727dbdd5471'
+        'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=77405a385bebafa9f315f727dbdd5471'
     )
         .then(function (response) {
             return response.json();
         })
-        .then(function (response) {
-            console.log(response);
+        .then(function (weather) {
+            mainTempEl.innerHTML= "Temperature: " + Math.round(weather.main.temp) +"&deg F";
+            console.log(weather.main.temp);
         });
 }
 
-currentWeather("Orlando");
+currentWeather("San Diego");
+
