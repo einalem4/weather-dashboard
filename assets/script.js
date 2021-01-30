193801476af1d41760d8eb4f2439b96afcc5adad
einalem4 <melanie@codehangar.io>
2021-01-29 22:12:31 -0700

    add icons and uv index

--- assets/script.js
+++ assets/script.js
@@ -13,17 +13,23 @@ function currentWeather(city) {
             return response.json();
         })
         .then(function (weather) {
-            mainTempEl.innerHTML = "Temperature: " + Math.round(weather.main.temp) + "&deg F";
+            mainTempEl.innerHTML = "Temperature: " + Math.round(weather.main.temp) + " &degF";
             document.getElementById("humidity").innerHTML = "Humidity: " + weather.main.humidity + "%";
             document.getElementById("wind").innerHTML = "Wind Speed: " + weather.wind.speed + " MPH";
-
+            document.getElementById("city-name").innerHTML = "<h3>" + weather.name + "</h3>"
+            var currentIcon = document.getElementById("current-icon")
+            currentIcon.setAttribute('src', 'http://openweathermap.org/img/wn/' + weather.weather[0].icon + '@2x.png');
+           
+            uvIndex(weather);
         });
 }
 
-currentWeather("Seattle");
+currentWeather("San Diego");
 
+// UV index
 function uvIndex(weather) {
-
+    console.log(weather)
+    console.log(weather.coord)
     fetch(
         'https://api.openweathermap.org/data/2.5/uvi?lat=' + weather.coord.lat + '&lon=' + weather.coord.lon + '&appid=77405a385bebafa9f315f727dbdd5471'
     )
@@ -31,15 +37,13 @@ function uvIndex(weather) {
             return response.json();
         })
         .then(function (response) {
-            document.getElementById("uv").innerHTML = "UV Index:" + uvIndex.value
+            document.getElementById("uv").innerHTML = "UV Index: " + response.value
             console.log(response);
             console.log(weather);
-            console.log(uvIndex(weather));
         });
-
-    uvIndex(weather);
 }
 
+// five day forecast
 function forecast(city) {
     fetch(
         'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=77405a385bebafa9f315f727dbdd5471'
@@ -49,19 +53,36 @@ function forecast(city) {
             return response.json();
         })
         .then(function (weather) {
-            document.getElementById("temp-one").innerHTML = "Temperature: " + Math.round(weather.list[0].main.temp) + "&deg F";
+
+            document.getElementById("temp-one").innerHTML = "Temperature: " + Math.round(weather.list[0].main.temp) + " &degF";
             document.getElementById("humidity-one").innerHTML = "Humidity: " + weather.list[0].main.humidity + "%";
-            document.getElementById("temp-two").innerHTML = "Temperature: " + Math.round(weather.list[1].main.temp) + "&deg F";
+            var iconOne = document.getElementById("icon-one")
+            iconOne.setAttribute('src', 'http://openweathermap.org/img/wn/' + weather.list[0].weather[0].icon + '@2x.png');
+
+
+            document.getElementById("temp-two").innerHTML = "Temperature: " + Math.round(weather.list[1].main.temp) + " &degF";
             document.getElementById("humidity-two").innerHTML = "Humidity: " + weather.list[1].main.humidity + "%";
-            document.getElementById("temp-three").innerHTML = "Temperature: " + Math.round(weather.list[2].main.temp) + "&deg F";
+            var iconTwo = document.getElementById("icon-two")
+            iconTwo.setAttribute('src', 'http://openweathermap.org/img/wn/' + weather.list[1].weather[0].icon + '@2x.png');
+
+            document.getElementById("temp-three").innerHTML = "Temperature: " + Math.round(weather.list[2].main.temp) + " &degF";
             document.getElementById("humidity-three").innerHTML = "Humidity: " + weather.list[2].main.humidity + "%";
-            document.getElementById("temp-four").innerHTML = "Temperature: " + Math.round(weather.list[3].main.temp) + "&deg F";
+            var iconThree = document.getElementById("icon-three")
+            iconThree.setAttribute('src', 'http://openweathermap.org/img/wn/' + weather.list[2].weather[0].icon + '@2x.png');
+
+            document.getElementById("temp-four").innerHTML = "Temperature: " + Math.round(weather.list[3].main.temp) + " &degF";
             document.getElementById("humidity-four").innerHTML = "Humidity: " + weather.list[3].main.humidity + "%";
-            document.getElementById("temp-five").innerHTML = "Temperature: " + Math.round(weather.list[4].main.temp) + "&deg F";
+            var iconFour = document.getElementById("icon-four")
+            iconFour.setAttribute('src', 'http://openweathermap.org/img/wn/' + weather.list[3].weather[0].icon + '@2x.png');
+
+            document.getElementById("temp-five").innerHTML = "Temperature: " + Math.round(weather.list[4].main.temp) + " &degF";
             document.getElementById("humidity-five").innerHTML = "Humidity: " + weather.list[4].main.humidity + "%";
+            var iconFive = document.getElementById("icon-five")
+            iconFive.setAttribute('src', 'http://openweathermap.org/img/wn/' + weather.list[4].weather[0].icon + '@2x.png');
 
         });
+
 }
-forecast("Seattle");
+forecast("San Diego");
 
 
